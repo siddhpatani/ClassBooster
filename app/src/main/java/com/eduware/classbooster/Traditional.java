21b2867f9fee363be63d6963c781afe912609b19
ukshat <33008157+ukshat@users.noreply.github.com>
2020-06-13 19:38:43 -0700

    Traditional classroom : algorithm completed, some tests written

--- app/src/main/java/com/eduware/classbooster/Traditional.java
+++ app/src/main/java/com/eduware/classbooster/Traditional.java
@@ -1,15 +1,26 @@
 package com.eduware.classbooster;
-import java.util.*;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class Traditional extends Classroom {
 
-    private Student [][] chart;
+    private final Student[][] chart;
 
     // precondition: students.size() >= depth
-    public Traditional(double span, double depth) {
-//        double ratio = span/depth;
-        // TODO Si      ze chart based on span and depth
-        chart = new Student[7][5];
+    public Traditional(int studentCount, double span, double depth) {
+        double skew = (double) studentCount / span / depth;
+        final int rows = (int) Math.ceil(span * Math.sqrt(skew));
+        final int cols = (int) Math.ceil(depth * Math.sqrt(skew));
+        chart = new Student[rows][cols];
+    }
+
+    public int getRowCount() {
+        return chart.length;
+    }
+
+    public int getColCount() {
+        return chart[0].length;
     }
 
     // postcondition: students.size() > 0
@@ -18,20 +29,20 @@ public class Traditional extends Classroom {
         Random r = new Random();
         // Make a copy of students in a randomized order so that we don't damage the list as we assign them to seats
         final ArrayList<Student> randStudents = new ArrayList<Student>(students.size());
-        for(int i = 0; i < students.size(); i++){
-            int randomNumber=r.nextInt(students.size());
+        for (int i = 0; i < students.size(); i++) {
+            int randomNumber = r.nextInt(students.size());
             randStudents.set(i, students.get(randomNumber));
         }
 
-        for(int col = 0; col < chart[0].length; col++){
+        for (int col = 0; col < chart[0].length; col++) {
             int selection = r.nextInt(randStudents.size());
             chart[0][col] = randStudents.remove(selection);
         }
 
         boolean eyesightMatters = true;
-        for(int row = 0; row < chart.length; row++){
-            for(int col = 1; col < chart[0].length; col++){
-                chart[row][col] = this.findPartner(randStudents, chart[row][col - 1],  eyesightMatters);
+        for (int row = 0; row < chart.length; row++) {
+            for (int col = 1; col < chart[0].length; col++) {
+                chart[row][col] = this.findPartner(randStudents, chart[row][col - 1], eyesightMatters);
                 if (chart[row][col] == null) {
                     if (randStudents.size() > 0 && row < 2) {
                         chart[row][col] = this.findPartner(randStudents, chart[row][col - 1], false);
