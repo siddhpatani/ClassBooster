76780ecc6c725991293619eb19687f64f26a8c6f
ParthHathalia <66705867+ParthHathalia@users.noreply.github.com>
2020-06-17 23:13:47 -0700

    Added constuctors on Student class

--- app/src/main/java/com/eduware/classbooster/RatioOfRoom.java
+++ app/src/main/java/com/eduware/classbooster/RatioOfRoom.java
@@ -2,6 +2,7 @@ package com.eduware.classbooster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -52,6 +53,8 @@ public class RatioOfRoom extends AppCompatActivity {
                 final ClassBoosterApp globalVariable = (ClassBoosterApp) getApplicationContext();
 
                 globalVariable.setRoomInfo(ri);
+
+                startActivity(new Intent(RatioOfRoom.this, AddStudentActivity.class));
 
             }
         });
